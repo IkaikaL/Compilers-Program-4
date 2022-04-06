7cf722bf4b03ba9b0020c2daca4f15e02721bf73
IkaikaL <willtolivehaver@gmail.com>
2022-04-06 17:37:41 -0500

    deez

---
+++ Semant/LoopVarEntry.java
@@ -0,0 +1,12 @@
+package Semant;
+
+class LoopVarEntry extends VarEntry {
+  
+  LoopVarEntry(Types.Type t) {
+    super(t);
+  }
+
+  LoopVarEntry(Translate.Access a, Types.Type t) {
+    super(a, t);
+}
+}
---
+++ Semant/VarEntry.java
@@ -0,0 +1,15 @@
+package Semant;
+
+public class VarEntry extends Entry {
+  Translate.Access access;
+  public Types.Type ty;
+  
+  VarEntry(Types.Type t) {
+    ty = t;
+  }
+
+  VarEntry(Translate.Access a, Types.Type t) {
+    access = a;
+    ty = t;
+}
+}
