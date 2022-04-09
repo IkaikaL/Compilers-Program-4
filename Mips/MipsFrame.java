772bc047a3dc0a2763c2ad05a0a7450383982c95
IkaikaL <willtolivehaver@gmail.com>
2022-04-08 19:02:52 -0500

    fixes

--- Mips/MipsFrame.java
+++ Mips/MipsFrame.java
@@ -10,6 +10,8 @@ import Frame.AccessList;
 public class MipsFrame extends Frame {
 
   private int count = 0;
+  private int offset = 0;
+  
   public Frame newFrame(Symbol name, Util.BoolList formals) {
     Label label;
     if (name == null)
@@ -23,11 +25,36 @@ public class MipsFrame extends Frame {
 
   public MipsFrame() {}
   private MipsFrame(Label n, Util.BoolList f) {
+
     name = n;
+    // Process list
+    formals = allocFormals(0, f);
   }
 
   private static final int wordSize = 4;
   public int wordSize() { return wordSize; }
 
-  public Access allocLocal(boolean escape) { return null; }
+  public Access allocLocal(boolean escape) { 
+    if (escape) {
+      offset -= 4;
+      return new InFrame(offset);
+    }
+    else
+      return new InReg(new Temp());
+  }
+
+  public AccessList allocFormals(int offset, Util.BoolList formals) {
+    Access access;
+    if(formals == null) {
+      return null;
+    } else if (formals.head) {
+      // If there is a formal add a InFrame
+      access = new InFrame(offset);
+    } else {
+      // Otherwise add InReg
+      access = new InReg(new Temp());
+    }
+    
+    return new AccessList(access, allocFormals(offset + wordSize, formals.tail));
+  }
 }
---
+++ ReadMe.txt
@@ -0,0 +1,10 @@
+Ikaika Lee and Adam Dupuy
+
+Made modifications to a couple functions in Semant.Semant notably fixing the 1st pass in transDec.
+Added a new level for translation before the transExp call in transProg.
+Finished FindEscape.FindEscape to catch the variables that escape.
+Added code for allocating formal parameters in Mips.MipsFrame.
+
+There seems to be issues with INT based on our runs but we are unsure of what could be causing it
+but everything else seems to be working as intended.  For this project we used the book and followed the
+assignment pdf as closely as possible.
